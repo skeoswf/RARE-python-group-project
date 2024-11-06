40145ad91f51d7e16cb1f9d24ddfd94e7507f769
Cody Keener <codymkeener@gmail.com>
2024-11-05 19:59:41 -0600

    Created models for tag and post_tag, added tag_requests for get_all_tags, get_single_tag, and get_tag_by_post, and added those to the do_GET method in the request_handler

---
+++ views/tag_requests.py
@@ -0,0 +1,76 @@
+import sqlite3
+from models import Tag
+
+def get_all_tags():
+  with sqlite3.connect("./rare.sqlite3") as conn:
+    
+    conn.row_factory = sqlite3.Row
+    db_cursor = conn.cursor()
+    
+    db_cursor.execute("""
+    SELECT
+      t.id,
+      t.label
+    FROM Tags t
+    """)
+    
+    tags = []
+    
+    dataset = db_cursor.fetchall()
+    
+    for row in dataset:
+      
+      tag = Tag(row['id'], row['label'])
+      
+      tags.append(tag.__dict__)
+      
+  return tags
+
+def get_single_tag(id):
+  with sqlite3.connect("./rare.sqlite3") as conn:
+    
+    conn.row_factory = sqlite3.Row
+    db_cursor = conn.cursor()
+    
+    db_cursor.execute("""
+    SELECT
+      t.id,
+      t.label
+    FROM Tags t
+    WHERE t.id = ?
+    """, (id, ))
+    
+    data = db_cursor.fetchone()
+      
+    tag = Tag(data['id'], data['label'])
+      
+  return tag.__dict__
+
+def get_tags_by_post(id):
+  with sqlite3.connect("./rare.sqlite3") as conn:
+    
+    conn.row_factory = sqlite3.Row
+    db_cursor = conn.cursor()
+    
+    db_cursor.execute("""
+    SELECT
+      t.id,
+      t.label,
+      pt.post_id,
+      pt.tag_id
+    FROM Tags t
+    JOIN PostTags pt
+      ON t.id = pt.tag_id
+    WHERE pt.post_id = ?
+    """, (id, ))
+    
+    tags = []
+    
+    dataset = db_cursor.fetchall()
+    
+    for row in dataset:
+      tag = Tag(row['id'], row['label'])
+      
+      tags.append(tag.__dict__)
+      
+  return tags
